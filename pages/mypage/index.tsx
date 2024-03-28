8696aa4187d9243ef524737cbdec740d2b46389d
hajung00 <gkwjd719@naver.com>
2024-03-29 02:18:21 +0900

    build 에러 수정

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -2,6 +2,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { GetServerSideProps } from 'next';
+import { styled } from 'styled-components';
 
 // import svg
 import Beans from '../../public/assets/beans.svg';
@@ -16,8 +17,133 @@ import Footer from '../../components/Footer';
 import Layout from '../../components/Layout';
 import { getCookieValue } from '../../func/getCookieValue';
 
-// import styles
-import { MypageWrapper, PageWrapper, User, UserInfo } from './styles';
+export const MypageWrapper = styled.div`
+  .sub-title {
+    color: var(--Brand-Color, #2c2310);
+    font-family: 'Pretendard';
+    font-size: 22px;
+    font-weight: 600;
+  }
+`;
+
+export const PageWrapper = styled.div`
+  height: calc(100vh - 80px - 78px);
+  overflow-y: auto;
+  overflow-x: hidden;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: #ccc;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: gray;
+  }
+  &::-webkit-scrollbar-button {
+    display: none;
+  }
+`;
+
+export const User = styled.div`
+  padding: 16px;
+  padding-top: 57px;
+  .img-wrapper {
+    width: 74px;
+    height: 74px;
+    background: #fff;
+    border-radius: 50%;
+  }
+  .nickname {
+    margin-top: 24px;
+    display: block;
+    position: relative;
+
+    &::after {
+      content: '';
+      display: block;
+      height: 1px;
+      background: #d8c6b7;
+      position: absolute;
+      top: 0;
+      width: calc(100% - 105px);
+      top: 13px;
+      left: 102px;
+    }
+  }
+
+  .taste-wrapper {
+    .text {
+      color: var(--Brand-Color, #2c2310);
+      font-family: 'Pretendard';
+      font-size: 14px;
+      font-weight: 500;
+    }
+    .content {
+      margin-top: 22px;
+    }
+    .non-content {
+      margin-top: 30px;
+    }
+    .tag-wrapper {
+      display: flex;
+      height: 24px;
+      gap: 8px;
+      margin-top: 12px;
+      width: 100%;
+      flex-flow: wrap;
+
+      .tag {
+        width: fit-content;
+        background: #fff;
+        border-radius: 4px;
+        display: flex;
+        height: 24px;
+        padding: 4px 6px;
+        justify-content: center;
+        align-items: center;
+        gap: 4px;
+        font-family: 'Pretendard';
+        font-size: 14px;
+        font-weight: 500;
+      }
+      .strength {
+        color: #5b4132;
+      }
+      .acidity {
+        color: #8d6949;
+      }
+    }
+  }
+`;
+export const UserInfo = styled.div`
+  padding: 16px;
+  margin-bottom: 38px;
+  .info {
+    margin-top: 38px;
+    padding: 18px 0;
+    border-bottom: 4px solid #2c2310;
+  }
+
+  .select-list-wrapper {
+    .list {
+      color: var(--Brand-Color, #2c2310);
+      font-family: 'Pretendard';
+      font-size: 16px;
+      font-weight: 500;
+      padding: 21px 0;
+      border-bottom: 1px solid #d4d4d4;
+      cursor: pointer;
+
+      &:hover {
+        background: var(--Brand-50, #efe3d8);
+      }
+    }
+    .list:last-child {
+      border-bottom: none;
+    }
+  }
+`;
 
 interface Props {
   user: string;
--- pages/mypage/select/index.tsx
+++ pages/mypage/select/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { styled } from 'styled-components';
 
 // import svg
 import Beans from '../../../public/assets/beans.svg';
@@ -13,9 +14,129 @@ import Footer from '../../../components/Footer';
 import Layout from '../../../components/Layout';
 import { getCookieValue } from '../../../func/getCookieValue';
 
-//import styles
-import { PageWrapper } from '../styles';
-import { ButtonSection, PageTitle, SelectSection, TagItem } from './styles';
+export const PageWrapper = styled.div`
+  height: calc(100vh - 80px - 78px);
+  overflow-y: auto;
+  overflow-x: hidden;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: #ccc;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: gray;
+  }
+  &::-webkit-scrollbar-button {
+    display: none;
+  }
+`;
+export const PageTitle = styled.div`
+  display: block;
+  padding: 0 16px;
+  position: relative;
+  color: var(--Brand-Color, #2c2310);
+  font-family: 'Pretendard';
+  font-size: 22px;
+  font-weight: 600;
+  margin-top: 57px;
+
+  &::after {
+    content: '';
+    display: block;
+    height: 1px;
+    background: #d8c6b7;
+    position: absolute;
+    top: 0;
+    width: calc(100% - 210px);
+    top: 13px;
+    left: 193px;
+  }
+`;
+
+export const SelectSection = styled.div`
+  padding: 0 16px;
+  margin-top: 62px;
+
+  .title-wrapper {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    margin-bottom: 20px;
+
+    .title {
+      color: var(--Brand-Color, #2c2310);
+      font-family: 'Pretendard';
+      font-size: 18px;
+      font-weight: 600;
+    }
+    .description {
+      color: #636363;
+      font-family: 'Pretendard';
+      font-size: 12px;
+      font-weight: 500;
+    }
+  }
+
+  .tag-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    .column {
+      display: flex;
+      flex-flow: wrap;
+      gap: 12px;
+    }
+  }
+  .flex {
+    flex-direction: row;
+    flex-flow: wrap;
+  }
+`;
+
+export const TagItem = styled.div<{ background: string; color: string }>`
+  background: ${(props) => (props.background ? props.background : '#fff')};
+  color: ${(props) => (props.color ? props.color : '#A5A5A5')};
+  font-family: 'Pretendard';
+  font-size: 18px;
+  font-weight: 500;
+  padding: 6px 9px;
+  border-radius: 6px;
+  width: fit-content;
+  min-width: fit-content;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+`;
+
+export const ButtonSection = styled.div`
+  padding: 0 16px;
+  height: 58px;
+  margin-top: 62px;
+  margin-bottom: 30px;
+
+  & > button {
+    height: 58px;
+    border: none;
+    border-radius: 8px;
+    color: #fff;
+    background: var(--Gray-100, #ccc);
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    font-family: 'Pretendard';
+    font-size: 16px;
+    font-weight: 500;
+  }
+  .active {
+    cursor: pointer;
+    background: #5d4c21;
+    color: #fff;
+  }
+`;
 
 interface Props {
   user: string;
