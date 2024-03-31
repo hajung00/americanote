88aa136ea05f9d4fb3e3b7f3ea663addf17fe636
hajung00 <gkwjd719@naver.com>
2024-03-31 15:20:10 +0900

    modallayout 스타일 변경

--- components/ModalLayout/index.tsx
+++ components/ModalLayout/index.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const ModalLayoutStyle = styled.div`
   width: 100%;
-  height: 100vh;
+  height: -webkit-fill-available;
   background: rgba(0, 0, 0, 0.5);
   position: absolute;
   top: 0;
