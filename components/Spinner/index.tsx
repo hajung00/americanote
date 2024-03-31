46bcf649bf62c173de9e5269f849683f52ce9c3a
hajung00 <gkwjd719@naver.com>
2024-03-31 12:45:38 +0900

    로딩 스피너 디자인 변경

--- components/Spinner/index.tsx
+++ components/Spinner/index.tsx
@@ -13,7 +13,7 @@ const SpinnerContainer = styled.div`
   position: absolute;
   z-index: 999;
   align-items: center;
-  width: 475px;
+  width: 100%;
   height: 100vh;
   justify-content: center;
   background: rgba(0, 0, 0, 0.5);
